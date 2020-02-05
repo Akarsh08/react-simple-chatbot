99dbc92f20061c4dff6389a7b242740d5df9029f
Akarsh Srivastava <akarsh@Akarshs-MBP.blr-office.geekyants.com>
2020-02-05 17:28:18 +0530

    fixed: avatar aspect ratio problem

--- lib/steps_components/text/Image.jsx
+++ lib/steps_components/text/Image.jsx
@@ -5,12 +5,11 @@ const Image = styled.img`
   animation: ${scale} 0.3s ease forwards;
   border-radius: ${props => (props.user ? '50% 50% 50% 50%' : '50% 50% 50% 50%')};
   box-shadow: rgba(0, 0, 0, 0.15) 0px 1px 2px 0px;
-  height: 40px;
-  width: 40px;
+  max-height: 40px;
+  max-width: 40px;
   padding: 3px;
   transform: scale(0);
   transform-origin: ${props => (props.user ? 'bottom left' : 'bottom right')};
-  width: 40;
 `;
 
 export default Image;
